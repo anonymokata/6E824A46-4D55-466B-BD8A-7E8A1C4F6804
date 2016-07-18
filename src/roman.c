48368d93f9bbe713d7cd1fbfff7a2c20597c4778
anonymous <anonymous>
2016-07-18 11:17:09 -0600

    Refactored the code. Added helper function to concatenate two strings into a newly allocated string, with error checking.

--- src/roman.c
+++ src/roman.c
@@ -3,15 +3,24 @@
 #include <string.h>
 #include "roman.h"
 
-char *add_roman_numerals(char *augend, char *addend) {
-  size_t length_augend = strlen(augend);
-  size_t length_addend = strlen(addend);
-  size_t length_result = length_addend + length_augend;
-  char *result = malloc((length_result + 1) * sizeof(*(result)));
+char *concatinate_strings(char *starting_string, char *ending_string) {
+  char *result;
+  size_t length_starting_string = strlen(starting_string);
+  assert(length_starting_string > 0);
+  size_t length_ending_string = strlen(ending_string);
+  assert(length_ending_string > 0);
+  size_t length_result = length_starting_string + length_ending_string;
+  assert(length_result > 0);
+  result = malloc((length_result + 1) * sizeof(*(result)));
   assert(result != NULL);
   result[0] = '\0'; // ensure string starts with a null byte for strcat.
-  strcat(result, augend);
-  strcat(result, addend);
+  strcat(result, starting_string);
+  strcat(result, ending_string);
+  return result;
+}
+
+char *add_roman_numerals(char *augend, char *addend) {
+  char *result = concatinate_strings(augend, addend);
   if (strcmp(result, "IIII") == 0) {
     strcpy(result, "IV");
   }
