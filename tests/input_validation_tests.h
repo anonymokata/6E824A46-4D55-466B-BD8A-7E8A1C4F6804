82684ed74086efcdc67a4e6e1f7f1ff847e497b1
anonymous <anonymous>
2016-07-28 11:22:10 -0600

    Added a test for MMM being a valid numeral

--- tests/input_validation_tests.h
+++ tests/input_validation_tests.h
@@ -102,6 +102,13 @@ START_TEST(test_CM_is_valid) {
 }
 END_TEST
 
+START_TEST(test_MMM_is_valid) {
+  bool result;
+  result = validate_roman_numeral("MMM");
+  ck_assert(result == true);
+}
+END_TEST
+
 Suite *input_validation_suite(void) {
   Suite *s;
   TCase *tc_core;
@@ -123,6 +130,7 @@ Suite *input_validation_suite(void) {
   tcase_add_test(tc_core, test_D_is_valid);
   tcase_add_test(tc_core, test_CD_is_valid);
   tcase_add_test(tc_core, test_CM_is_valid);
+  tcase_add_test(tc_core, test_MMM_is_valid);
   suite_add_tcase(s, tc_core);
 
   return s;
