da462e61b6e8f3dc1640fb56ae0b6a6556fd9d55
anonymous <anonymous>
2016-07-28 11:14:04 -0600

    Added a test for XC being a valid roman numeral and passing code.

--- tests/input_validation_tests.h
+++ tests/input_validation_tests.h
@@ -67,6 +67,13 @@ START_TEST(test_XL_is_valid) {
 }
 END_TEST
 
+START_TEST(test_XC_is_valid) {
+  bool result;
+  result = validate_roman_numeral("XC");
+  ck_assert(result == true);
+}
+END_TEST
+
 Suite *input_validation_suite(void) {
   Suite *s;
   TCase *tc_core;
@@ -83,6 +90,7 @@ Suite *input_validation_suite(void) {
   tcase_add_test(tc_core, test_XXX_is_valid);
   tcase_add_test(tc_core, test_L_is_valid);
   tcase_add_test(tc_core, test_XL_is_valid);
+  tcase_add_test(tc_core, test_XC_is_valid);
   suite_add_tcase(s, tc_core);
 
   return s;
